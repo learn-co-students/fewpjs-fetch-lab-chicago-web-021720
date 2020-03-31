798b6bea3f0651483847513361566b6518cb70ea
ellisandrews <ellisandrews1@gmail.com>
2020-03-31 14:46:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
-
+  return fetch(
+    'https://anapioficeandfire.com/api/books'
+  ).then(
+    response => response.json()
+  ).then(
+    json => renderBooks(json)
+  )
 }
 
 function renderBooks(json) {
